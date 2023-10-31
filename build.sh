359e368b9169399cc4fe6f134f6488c7bc38e051
Stan van Dijk <536076@student.saxion.nl>
2023-10-31 09:40:48 +0100

    Test with build command

--- build.sh
+++ build.sh
@@ -79,11 +79,24 @@ GLOBAL_CSS="html, body {
 
     button:focus {
         border-color: #666;
-    }"
+    }
+"
 
-FAVICON="./favicon.png"
-
+# The public folder is cleared and the site is built
 rm -rf ./public
 pnpm build
 echo "$HTML" >./public/index.html
 echo "$GLOBAL_CSS" >./public/global.css
+
+# The content in public is copied to the root of the site
+cp -r ./public/* ./
+
+# Add the files to git
+git add .
+[ -z "$1" ] && git commit -m "Update" || git commit -m "$1"
+git push
+
+# Remove the files from the root of the site
+rm index.html
+rm global.css
+rm -rf build
